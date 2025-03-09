162289673bd91228209adf9d69160accd16b5b29
ammar <amarya211fgff@gmail.com>
2025-03-10 01:38:05 +0200

    validate date from request befor store it in database and show message if found an invalid date

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Offer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CrudController extends Controller
 {
@@ -36,6 +37,15 @@ class CrudController extends Controller
     public function store(Request $request){
         // validate data
 
+        $rules = $this->getRules();
+        $messages = $this->getMessages();
+        $validator = Validator::make($request->all(), $rules , $messages);
+
+
+        if($validator->fails()){
+            return $validator->errors();
+//            return $validator->errors()->first();
+        }
         //insert data
 
         Offer::create([
@@ -47,4 +57,21 @@ class CrudController extends Controller
         return 'saved';
     }
 
+    protected function getRules(){
+        return  $rules =[
+            'offer_name' => 'required|max:100|unique:offers,offer_name',
+            'offer_price' => 'required|numeric',
+            'offer_details' => 'required',
+        ];
+    }
+    protected function getMessages()
+    {
+        return $messages = [
+            'offer_name.required'=>'offer name must have vlaue',
+            'offer_name.unique'=>'offer name already exists',
+            'offer_price.required'=>'offer price must have value',
+            'offer_price.numeric'=>'offer price must be numeric',
+            'offer_details.required'=>'offer details must have value',
+        ];
+    }
 }
